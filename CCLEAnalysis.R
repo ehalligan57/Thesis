460f76172bd0ef0c8d2770c6b7f296d3b26efe62
Caitlin Halligan <caitlin.halligan@gmail.com>
2021-03-19 12:28:31 -0700

    More cleanup

--- CCLEAnalysis.R
+++ CCLEAnalysis.R
@@ -7,17 +7,21 @@ library(ggplot2)
 
 ########## Load Data ##########
 
-RNAseqcounts <- fread("/Users/caitlinhalligan/Desktop/Thesis/CCLE_RNAseq_genes_counts_20180929.gct") 
+#Setting working directory for either of our computers. Uncomment the line for your computer
+setwd("/Users/sdalin/Dropbox (Partners HealthCare)/Postdoc/Projects/CCNU/Caitlin/Thesis")
+#setwd("/Users/caitlinhalligan/Desktop/Thesis")
+
+RNAseqcounts <- fread("./data/CCLE_RNAseq_genes_counts_20180929.gct") 
 RNAseqcountsGlioma <- select(RNAseqcounts,Name, Description,contains("CENTRAL_NERVOUS_SYSTEM"))
-Methylation <- fread("/Users/caitlinhalligan/Desktop/Thesis/CCLE_RRBS_TSS1kb_20181022.txt") 
+Methylation <- fread("./data/CCLE_RRBS_TSS1kb_20181022.txt") 
 # Contains experiment_id (convert to cell line name) and master_cpd_id (convert to drug name)#
-Sensitivity <- fread("/Users/caitlinhalligan/Desktop/Thesis/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.data.curves_post_qc.txt") 
+Sensitivity <- fread("./data/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.data.curves_post_qc.txt") 
 # File has master_ccl_id and ccl_name #
-CellLineMeta <- fread("/Users/caitlinhalligan/Desktop/Thesis/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.meta.per_cell_line.txt") 
+CellLineMeta <- fread("./data/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.meta.per_cell_line.txt") 
 # File has master_cpd_id to cdp_name (the drug name) #
-CompoundMeta <- fread("/Users/caitlinhalligan/Desktop/Thesis/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.meta.per_compound.txt") 
+CompoundMeta <- fread("./data/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.meta.per_compound.txt") 
 # File has experiment_id and master_ccl_id #
-ExperimentMeta <- fread("/Users/caitlinhalligan/Desktop/Thesis/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.meta.per_experiment.txt") 
+ExperimentMeta <- fread("./data/CTRPv2.0_2015_ctd2_ExpandedDataset/v20.meta.per_experiment.txt") 
 
 ########## Filter for MGMT Data ##########
 MGMTexpression <- as.data.frame(t(RNAseqcounts[which(RNAseqcounts$Description=="MGMT"),])) 
@@ -162,6 +166,7 @@ for(ColNumber in (3:ncol(EdgeRdataframe))){#loops over drugs
   for(RowNumber in (1:nrow(EdgeRdataframe))){ #loops of cell lines
     if (EdgeRdataframe$MGMTexpressiondata[RowNumber]=='high'){
       if (is.na(EdgeRdataframe[RowNumber,ColNumber])){ #skip NAs
+        next #This is the "official" command to skip to the next iteration of a for or while loop. It does work without it too.
       }else{
         new_sensitivity <- EdgeRdataframe[RowNumber,ColNumber]
         MGMThighSensitivity <- c(MGMThighSensitivity, new_sensitivity)
@@ -176,15 +181,48 @@ for(ColNumber in (3:ncol(EdgeRdataframe))){#loops over drugs
   }
   new_designMatMGMThigh <- model.matrix(~0+MGMThighSensitivity) #this works, same as old one
   new_designMatMGMTlow <- model.matrix(~0+MGMTlowSensitivity) #this works, same as old one
-  ListofDesignMatricesMGMThigh[[ColNumber]] <- new_designMatMGMThigh
-  ListofDesignMatricesMGMTlow[[ColNumber]] <- new_designMatMGMTlow
+  
+  #saying "ListofDesignMatricesMGMThigh[[ColNumber]]" was storing each
+  #design matrix into the nth + 2 slot, since the first colNumber
+  #you use was col #3! I changed it so they start at 1 now.
+  nextEmptySlot <- length(ListofDesignMatricesMGMThigh) + 1
+  
+  ListofDesignMatricesMGMThigh[[nextEmptySlot]] <- new_designMatMGMThigh
+  ListofDesignMatricesMGMTlow[[nextEmptySlot]] <- new_designMatMGMTlow
 } 
+
+#Name the slots of the lists of design matrices by the drug they refer to
+names(ListofDesignMatricesMGMThigh) <- as.vector(sapply(names(EdgeRdataframe)[3:ncol(EdgeRdataframe)], function(x){substr(x, 1, regexpr(" ", x)-1)}))
+names(ListofDesignMatricesMGMTlow) <- sapply(names(EdgeRdataframe)[3:ncol(EdgeRdataframe)], function(x){substr(x, 1, regexpr(" ", x)-1)})
+
+
+
 ListofDesignMatricesMGMThigh[[3]]==designMatChlorambucilMGMThigh 
 #it's the same for Chlorambucil, Dacarbazine, Temozolomide, Bendamustine, Platin, Cyclophosphamide, Oxaliplatin
 #it is wrong for Ifosfamide (5)
+
+#this is true for me:
+all(designMatIfosfamideMGMThigh == ListofDesignMatricesMGMThigh$ifosfamide)
+
 ListofDesignMatricesMGMTlow[[3]]==designMatChlorambucilMGMTlow
 #it's right for Dacarbazine, Temozolomide, Bendamustine, Platin, Cyclophosphamide, Oxaliplatin
 #it's wrong for Chlorambucil (3), Ifosfamide (5)
+
+#this is true for me:
+all(designMatChlorambucilMGMTlow == ListofDesignMatricesMGMTlow$chlorambucil)
+
+#This gives me an error:
+all(designMatIfosfamideMGMTlow == ListofDesignMatricesMGMTlow$ifosfamide)
+
+#They are different sizes:
+dim(designMatIfosfamideMGMTlow)
+dim(ListofDesignMatricesMGMTlow$ifosfamide)
+
+#There are 14 MGMT low cell lines with ifosphamide sensitivity data
+sum(!is.na(EdgeRdataframe$`ifosfamide SensitivityAUC`)[EdgeRdataframe$MGMTexpressiondata == 'low'])
+
+#So the new version with the for loop is correct. Why was the old version incorrect though?
+#There's a typo on line 290 where you set that design matrix referring to MGMT high, not low.
 
 
 # Design Matrix for MGMT high, chlorambucil # #28 total, 14 R, 14S #error message but it looks right?
@@ -195,10 +233,6 @@ ChlorambucilMGMThigh <- EdgeRdataframe %>%
 designMatChlorambucilMGMThigh <- model.matrix(~0+ChlorambucilMGMThigh$`chlorambucil SensitivityAUC`)
 
 # Design Matrix for MGMT low, chlorambucil #15 total, 8 R, 7 S
-#ChlorambucilMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#ChlorambucilMGMTlow <- data.frame(celllines=ChlorambucilMGMTlow$celllines,ChlorambucilMGMTlow = ChlorambucilMGMTlow$`chlorambucil SensitivityAUC`)
-#ChlorambucilMGMTlow <- na.omit(ChlorambucilMGMTlow)
-#designMatChlorambucilMGMTlow <- model.matrix(~ChlorambucilMGMTlow$ChlorambucilMGMTlow)
 ChlorambucilMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>%
   select(celllines,`chlorambucil SensitivityAUC`) %>%
@@ -206,10 +240,6 @@ ChlorambucilMGMTlow <- EdgeRdataframe %>%
 designMatChlorambucilMGMTlow <- model.matrix(~0+ChlorambucilMGMTlow$`chlorambucil SensitivityAUC`)
 
 # Design Matrix for MGMT high, dacarbazine # #28 total, 12 R, 16 S
-#DacarbazineMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#DacarbazineMGMThigh <- data.frame(celllines=DacarbazineMGMThigh$celllines,DacarbazineMGMThigh = DacarbazineMGMThigh$`dacarbazine SensitivityAUC`)
-#DacarbazineMGMThigh <- na.omit(DacarbazineMGMThigh)
-#designMatDacarbazineMGMThigh <- model.matrix(~DacarbazineMGMThigh$DacarbazineMGMThigh)
 DacarbazineMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>%
   select(celllines,`dacarbazine SensitivityAUC`) %>%
@@ -217,10 +247,6 @@ DacarbazineMGMThigh <- EdgeRdataframe %>%
 designMatDacarbazineMGMThigh <- model.matrix(~0+DacarbazineMGMThigh$`dacarbazine SensitivityAUC`)
 
 # Design Matrix for MGMT low, dacarbazine # #15 total, 10 R, 5 S
-#DacarbazineMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#DacarbazineMGMTlow <- data.frame(celllines=DacarbazineMGMTlow$celllines,DacarbazineMGMTlow = DacarbazineMGMTlow$`dacarbazine SensitivityAUC`)
-#DacarbazineMGMTlow <- na.omit(DacarbazineMGMTlow)
-#designMatDacarbazineMGMTlow <- model.matrix(~DacarbazineMGMTlow$DacarbazineMGMTlow)
 DacarbazineMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>%
   select(celllines,`dacarbazine SensitivityAUC`) %>%
@@ -228,11 +254,6 @@ DacarbazineMGMTlow <- EdgeRdataframe %>%
 designMatDacarbazineMGMTlow <- model.matrix(~0+DacarbazineMGMTlow$`dacarbazine SensitivityAUC`)
 
 # Design Matrix for MGMT high, ifosfamide # #13 total, 6 R, 7 S
-#IfosfamideMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#IfosfamideMGMThigh <- data.frame(celllines=IfosfamideMGMThigh$celllines,IfosfamideMGMThigh = IfosfamideMGMThigh$`ifosfamide SensitivityAUC`)
-#IfosfamideMGMThigh <- na.omit(IfosfamideMGMThigh)
-#designMatIfosfamideMGMThigh <- model.matrix(~IfosfamideMGMThigh$IfosfamideMGMThigh)
-
 IfosfamideMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>%
   select(celllines,`ifosfamide SensitivityAUC`) %>%
@@ -240,23 +261,13 @@ IfosfamideMGMThigh <- EdgeRdataframe %>%
 designMatIfosfamideMGMThigh <- model.matrix(~0+IfosfamideMGMThigh$`ifosfamide SensitivityAUC`)
 
 # Design Matrix for MGMT low, ifosfamide # #9 total, 5 R, 4 S
-#IfosfamideMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#IfosfamideMGMTlow <- data.frame(celllines=IfosfamideMGMTlow$celllines,IfosfamideMGMTlow = IfosfamideMGMTlow$`ifosfamide SensitivityAUC`)
-#IfosfamideMGMTlow <- na.omit(IfosfamideMGMTlow)
-#designMatIfosfamideMGMTlow <- model.matrix(~IfosfamideMGMTlow$IfosfamideMGMTlow)
-
 IfosfamideMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>%
   select(celllines,`ifosfamide SensitivityAUC`) %>%
   na.omit(`ifosfamide SensitivityAUC`)
-designMatIfosfamideMGMTlow <- model.matrix(~0+IfosfamideMGMThigh$`ifosfamide SensitivityAUC`)
+designMatIfosfamideMGMTlow <- model.matrix(~0+IfosfamideMGMThigh$`ifosfamide SensitivityAUC`) #typo here: referring to Ifosfamide high, not low.
 
 # Design Matrix for MGMT high, temozolomide # #28 total, 17 R, 11 S
-#TemozolomideMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#TemozolomideMGMThigh <- data.frame(celllines=TemozolomideMGMThigh$celllines,TemozolomideMGMThigh = TemozolomideMGMThigh$`temozolomide SensitivityAUC`)
-#TemozolomideMGMThigh <- na.omit(TemozolomideMGMThigh)
-#designMatTemozolomideMGMThigh <- model.matrix(~TemozolomideMGMThigh$TemozolomideMGMThigh)
-
 TemozolomideMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>%
   select(celllines,`temozolomide SensitivityAUC`) %>%
@@ -264,21 +275,14 @@ TemozolomideMGMThigh <- EdgeRdataframe %>%
 designMatTemozolomideMGMThigh <- model.matrix(~0+TemozolomideMGMThigh$`temozolomide SensitivityAUC`)
 
 # Design Matrix for MGMT low, temozolomide # #17 total, 9 R, 8 S
-#TemozolomideMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#TemozolomideMGMTlow <- data.frame(celllines=TemozolomideMGMTlow$celllines,TemozolomideMGMTlow = TemozolomideMGMTlow$`temozolomide SensitivityAUC`)
-#TemozolomideMGMTlow <- na.omit(TemozolomideMGMTlow)
-#designMatTemozolomideMGMTlow <- model.matrix(~TemozolomideMGMTlow$TemozolomideMGMTlow)
 TemozolomideMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>%
   select(celllines,`temozolomide SensitivityAUC`) %>%
   na.omit(`temozolomide SensitivityAUC`)
 designMatTemozolomideMGMTlow <- model.matrix(~0+TemozolomideMGMTlow$`temozolomide SensitivityAUC`)
 colnames(designMatTemozolomideMGMTlow) <- c("resistant","sensitive")
+
 # Design Matrix for MGMT high, bendamustine # #27 total, 14 R, 13 S
-#BendamustineMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#BendamustineMGMThigh <- data.frame(celllines=BendamustineMGMThigh$celllines,BendamustineMGMThigh = BendamustineMGMThigh$`bendamustine SensitivityAUC`)
-#BendamustineMGMThigh <- na.omit(BendamustineMGMThigh)
-#designMatBendamustineMGMThigh <- model.matrix(~BendamustineMGMThigh$BendamustineMGMThigh)
 BendamustineMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>%
   select(celllines,`bendamustine SensitivityAUC`) %>%
@@ -286,10 +290,6 @@ BendamustineMGMThigh <- EdgeRdataframe %>%
 designMatBendamustineMGMThigh <- model.matrix(~0+BendamustineMGMThigh$`bendamustine SensitivityAUC`)
 
 # Design Matrix for MGMT low, bendamustine # #17 total, 8 R, 9 S
-#BendamustineMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#BendamustineMGMTlow <- data.frame(celllines=BendamustineMGMTlow$celllines,BendamustineMGMTlow = BendamustineMGMTlow$`bendamustine SensitivityAUC`)
-#BendamustineMGMTlow <- na.omit(BendamustineMGMTlow)
-#designMatBendamustineMGMTlow <- model.matrix(~BendamustineMGMTlow$BendamustineMGMTlow)
 BendamustineMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>%
   select(celllines,`bendamustine SensitivityAUC`) %>%
@@ -297,10 +297,6 @@ BendamustineMGMTlow <- EdgeRdataframe %>%
 designMatBendamustineMGMTlow <- model.matrix(~0+BendamustineMGMTlow$`bendamustine SensitivityAUC`)
 
 # Design Matrix for MGMT high, Platin # #29 total, 15 R, 14 S
-#PlatinMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#PlatinMGMThigh <- data.frame(celllines=PlatinMGMThigh$celllines,PlatinMGMThigh = PlatinMGMThigh$`Platin SensitivityAUC`)
-#PlatinMGMThigh <- na.omit(PlatinMGMThigh)
-#designMatPlatinMGMThigh <- model.matrix(~PlatinMGMThigh$PlatinMGMThigh)
 PlatinMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>%
   select(celllines,`Platin SensitivityAUC`) %>%
@@ -308,10 +304,6 @@ PlatinMGMThigh <- EdgeRdataframe %>%
 designMatPlatinMGMThigh <- model.matrix(~0+PlatinMGMThigh$`Platin SensitivityAUC`)
 
 # Design Matrix for MGMT low, Platin # #17 total, 8 R, 9 S
-#PlatinMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#PlatinMGMTlow <- data.frame(celllines=PlatinMGMTlow$celllines,PlatinMGMTlow = PlatinMGMTlow$`Platin SensitivityAUC`)
-#PlatinMGMTlow <- na.omit(PlatinMGMTlow)
-#designMatPlatinMGMTlow <- model.matrix(~PlatinMGMTlow$PlatinMGMTlow)
 PlatinMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>%
   select(celllines,`Platin SensitivityAUC`) %>%
@@ -319,10 +311,6 @@ PlatinMGMTlow <- EdgeRdataframe %>%
 designMatPlatinMGMTlow <- model.matrix(~0+PlatinMGMTlow$`Platin SensitivityAUC`)
 
 # Design Matrix for MGMT high, cyclophosphamide # #
-#CyclophosphamideMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#CyclophosphamideMGMThigh <- data.frame(celllines=CyclophosphamideMGMThigh$celllines,CyclophosphamideMGMThigh = CyclophosphamideMGMThigh$`cyclophosphamide SensitivityAUC`)
-#CyclophosphamideMGMThigh <- na.omit(CyclophosphamideMGMThigh)
-#designMatCyclophosphamideMGMThigh <- model.matrix(~CyclophosphamideMGMThigh$CyclophosphamideMGMThigh)
 CyclophosphamideMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>% 
   select(celllines,`cyclophosphamide SensitivityAUC`) %>%
@@ -330,10 +318,6 @@ CyclophosphamideMGMThigh <- EdgeRdataframe %>%
 designMatCyclophosphamideMGMThigh <- model.matrix(~0+CyclophosphamideMGMThigh$`cyclophosphamide SensitivityAUC`)
 
 # Design Matrix for MGMT low, cyclophosphamide # #9 total, 4 R, 5 S
-#CyclophosphamideMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#CyclophosphamideMGMTlow <- data.frame(celllines=CyclophosphamideMGMTlow$celllines,CyclophosphamideMGMTlow = CyclophosphamideMGMTlow$`cyclophosphamide SensitivityAUC`)
-#CyclophosphamideMGMTlow <- na.omit(CyclophosphamideMGMTlow)
-#designMatCyclophosphamideMGMTlow <- model.matrix(~CyclophosphamideMGMTlow$CyclophosphamideMGMTlow)
 CyclophosphamideMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>% 
   select(celllines,`cyclophosphamide SensitivityAUC`) %>%
@@ -341,10 +325,6 @@ CyclophosphamideMGMTlow <- EdgeRdataframe %>%
 designMatCyclophosphamideMGMTlow <- model.matrix(~0+CyclophosphamideMGMTlow$`cyclophosphamide SensitivityAUC`)
 
 # Design Matrix for MGMT high, oxaliplatin # #28 total, 14 R, 14 S
-#OxaliplatinMGMThigh <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='high')
-#OxaliplatinMGMThigh <- data.frame(celllines=OxaliplatinMGMThigh$celllines,OxaliplatinMGMThigh = OxaliplatinMGMThigh$`oxaliplatin SensitivityAUC`)
-#OxaliplatinMGMThigh <- na.omit(OxaliplatinMGMThigh)
-#designMatOxaliplatinMGMThigh <- model.matrix(~OxaliplatinMGMThigh$OxaliplatinMGMThigh)
 OxaliplatinMGMThigh <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='high') %>% 
   select(celllines,`oxaliplatin SensitivityAUC`) %>%
@@ -352,10 +332,6 @@ OxaliplatinMGMThigh <- EdgeRdataframe %>%
 designMatOxaliplatinMGMThigh <- model.matrix(~0+OxaliplatinMGMThigh$`oxaliplatin SensitivityAUC`)
 
 # Design Matrix for MGMT low, oxaliplatin # #17 total, 8 R, 8 S
-#OxaliplatinMGMTlow <- EdgeRdataframe %>% filter(EdgeRdataframe$MGMTexpression=='low')
-#OxaliplatinMGMTlow <- data.frame(celllines=OxaliplatinMGMTlow$celllines,OxaliplatinMGMTlow = OxaliplatinMGMTlow$`oxaliplatin SensitivityAUC`)
-#OxaliplatinMGMTlow <- na.omit(OxaliplatinMGMTlow)
-#designMatOxaliplatinMGMTlow <- model.matrix(~OxaliplatinMGMTlow$OxaliplatinMGMTlow)
 OxaliplatinMGMTlow <- EdgeRdataframe %>%
   dplyr::filter(MGMTexpressiondata=='low') %>% 
   select(celllines,`oxaliplatin SensitivityAUC`) %>%
@@ -379,10 +355,6 @@ plotMDS(dgListGlioma) #what does this show again?
 # Setting up the model- try TMZ/MGMT low first
 designMatTemozolomideMGMTlow
 # Estimating Dispersons
-# why do the rows and col have to be the same?
-# estimates dispersions (how far apart the data is), 3 different methods
-# use common dispersions, an estimate based on mean-variance, genewise dispersion
-# takes into account dispersion when calculating significant differential expression
 dgListGliomaTMZMGMTlow <- estimateGLMCommonDisp(dgListGliomaTMZMGMTlow, design=designMatTemozolomideMGMTlow)
 #dispersion is same for each gene
 dgListGliomaTMZMGMTlow <- estimateGLMTrendedDisp(dgListGliomaTMZMGMTlow, design=designMatTemozolomideMGMTlow)
@@ -395,7 +367,6 @@ fit <- glmFit(dgListGliomaTMZMGMTlow, designMatTemozolomideMGMTlow)
 contrast_dgListGliomaTMZMGMTlow <- makeContrasts(TMZMGMTlow=resistant-sensitive,
                                                  levels=designMatTemozolomideMGMTlow)
 lrt <- glmLRT(fit, contrast=contrast_dgListGliomaTMZMGMTlow)
-#contrast vs co_eff?
 edgeR_result <- topTags(lrt)
 save(topTags(lrt,n=15000)$table, file='/Users/caitlinhalligan/Desktop/Thesis')
 deGenes <- decideTestsDGE(lrt, p=0.001)
